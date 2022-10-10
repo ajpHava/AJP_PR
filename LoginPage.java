662adca8087e4776962d71523ee93d28ae7b0fc7
ajpHava <113887498+ajpHava@users.noreply.github.com>
2022-10-10 12:24:04 +0530

    First Commit

--- LoginPage.java
+++ LoginPage.java
@@ -1,37 +1,66 @@
-import java.awt.Frame;
-import java.awt.Button;
 import java.awt.Label;
-import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginPage extends Frame{
-	Button loginButton ;
-	TextField userName , password ;
-	Label userNameLabel , passwordLabel;
-	public LoginPage(){
-		setVisible(true);
-		setSize(500,500);
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import java.awt.Container;
+
+public class LoginPage extends JFrame {
+	JButton loginButton;
+	JTextField userName;
+	JPasswordField password;
+	Label userNameLabel, passwordLabel;
+	Container container;
+
+	public LoginPage() {
+		setSize(500, 500);
 		setLayout(null);
-		
+		container = getContentPane();
+
 		userNameLabel = new Label("Username");
 		passwordLabel = new Label("Password");
-		userName = new TextField();
-		password = new TextField();
-		loginButton = new Button("Login");
-		
-		userNameLabel.setBounds(150 , 45 , 100 , 30);
-		userName.setBounds(260 , 50 , 100 , 20);
-		passwordLabel.setBounds(150, 85 , 100 , 30);
-		password.setBounds(260 , 90 , 100 , 20);
-		loginButton.setBounds(200 , 130 , 100 , 30);
-		
-		add(userNameLabel);
-		add(passwordLabel);
-		add(userName);
-		add(password);
-		add(loginButton);
-		
+		userName = new JTextField();
+		password = new JPasswordField();
+		password.setEchoChar('#');
+		loginButton = new JButton("Login");
+
+		userNameLabel.setBounds(150, 45, 100, 30);
+		userName.setBounds(260, 50, 100, 20);
+		passwordLabel.setBounds(150, 85, 100, 30);
+		password.setBounds(260, 90, 100, 20);
+		loginButton.setBounds(200, 130, 100, 30);
+
+		loginButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (new String(password.getPassword()).length() < 6) {
+					JOptionPane.showMessageDialog(null, "Password Length Should be greater than 6");
+					if (new String(password.getPassword()).equals("admin")) {
+						JOptionPane.showMessageDialog(null, "Login Done");
+					} else {
+						JOptionPane.showMessageDialog(null, "Login Failed");
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Enter password Properly");
+				}
+
+			}
+		});
+
+		container.add(userNameLabel);
+		container.add(passwordLabel);
+		container.add(userName);
+		container.add(password);
+		container.add(loginButton);
+
+		setVisible(true);
 	}
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		new LoginPage();
 	}
 }
